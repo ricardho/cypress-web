db9689cf434892a4e9d3ad9a4c3aa25d4de558b9
Ricardo Melo <ricardho@gmail.com>
2026-06-22 21:19:41 -0300

    inclusão report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
   e2e: {
     defaultCommandTimeout: 4000, // tempo padrão de espera
     baseUrl: 'https://automationpratice.com.br', // url base para os testes
